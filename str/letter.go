11719e133ad7681591d3fef81e70d69efdf6157a
潘晋锋 <1274196293@qq.com>
2024-10-16 10:14:21 +0800

    驼峰转下划线

--- str/letter.go
+++ str/letter.go
@@ -1,6 +1,9 @@
 package str
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 // Capitalize 将字符串的首字母转换为大写
 func Capitalize(str string) string {
@@ -18,4 +21,13 @@ func Lower(str string) string {
 	}
 	// 将首字母转换为大写，其余部分保持不变
 	return strings.ToLower(string(str[0])) + str[1:]
+}
+
+// ToSnakeCase 将驼峰字符串转换为下划线字符串
+func ToSnakeCase(str string) string {
+	// 使用正则表达式将驼峰字符串分割成单词
+	re := regexp.MustCompile("([a-z0-9])([A-Z])")
+	snake := re.ReplaceAllString(str, "${1}_${2}")
+	// 将字符串转换为小写
+	return strings.ToLower(snake)
 }
